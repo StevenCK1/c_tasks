e506079c88a5f34361627d9749c45748f588c714
StevenCK1 <stevencheung@hotmail.co.uk>
2023-01-28 12:39:24 +0000

    Task1

---
+++ Task1/Task1_FizzBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number: ");
+        int userNumber = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= userNumber; i++)
+        {
+            string output = "This is " + i + " out of " + userNumber;
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                output = "Fizz Buzz";
+            }
+            else if (i % 3 == 0)
+            {
+                output = "Fizz";
+            }
+            else if (i % 5 == 0)
+            {
+                output = "Buzz";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
