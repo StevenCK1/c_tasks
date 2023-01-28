79f8f4234ba3812ec796007a61f9c0289be95c2c
StevenCK1 <stevencheung@hotmail.co.uk>
2023-01-28 13:33:27 +0000

    comments added

--- Task2/Task2_CustomDictionary/Program.cs
+++ Task2/Task2_CustomDictionary/Program.cs
@@ -9,8 +9,8 @@ namespace Task2_CustomDictionary
         internal class MyDictionary
         {
             // private because variables are used inside of the constructors, variables would not be used externally
+            // Create variable "data" which is an array of keyValuePair, both key and value are strings
             private int arraySize = 100;
-            // Creat variable "data" which is an array of keyValuePair, both key and value are strings
             private KeyValuePair<string, string>[] data;
 
             // create public constructor called "MyDictionary" and initialize with KeyValuePair array
@@ -24,18 +24,25 @@ namespace Task2_CustomDictionary
             {
                 // Use the first character in the key to hash
                 // using remainder operator to ensure that the generated int is within the arraySize
+                // DO NOT USE THIS, poor hashing algo which will result in collisions
                 return (int)key[0] % arraySize;
             }
 
+            // create Add method to add key-value pair to dictionary
             public bool Add(string key, string value)
             {
+                // use Hash method to generate index in dictionary
+                // Add key-value pair add index and return true per H/w requirement
                 int index = Hash(key);
                 data[index] = new KeyValuePair<string, string>(key, value);
                 return true;
             }
 
+            //create Get method which take a string as key and return value based on the key
             public string Get(string key)
             {
+                // use Hash method to generate index in dictionary
+                // if index is not null then return value else return key not found
                 int index = Hash(key);
                 if (data[index].Key != null)
                     return data[index].Value;
@@ -43,8 +50,11 @@ namespace Task2_CustomDictionary
                     return "Key not found";
             }
 
+            // create remove method whcih takes a string and removes the key-value pair from the dictionary
             public bool Remove(string key)
             {
+                // use Hash method to generate index in dictionary
+                // if index is not null then replace the key-value pair with new empty KeyValuePair<string, string> object at index
                 int index = Hash(key);
                 if (data[index].Key != null)
                 {
