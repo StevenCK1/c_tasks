eb6119b8495b187e9981a6dd1333c6b218e4bb94
StevenCK1 <stevencheung@hotmail.co.uk>
2023-04-27 00:00:26 +0100

    added classes

---
+++ Task7/TradeAPI/Models/Pnl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeAPI.Models
+{
+    public class Pnl
+    {
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
