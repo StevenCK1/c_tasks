08609f8f71d8a1b53229d13e4cbcbccde29ff469
StevenCK1 <stevencheung@hotmail.co.uk>
2023-05-15 22:34:31 +0100

    removed in order to use scaffold

--- Task7/TradeAPI/Program.cs
+++ Task7/TradeAPI/Program.cs
@@ -5,9 +5,6 @@ using System.Runtime.CompilerServices;
 using TradeAPI.Models;
 using TradeAPI.Lib;
 using TradeAPI.TradesData;
-using Microsoft.Extensions.DependencyInjection;
-using TradeAPI.Db;
-using Microsoft.EntityFrameworkCore;
 
 class Program
 {
@@ -18,18 +15,6 @@ class Program
         CsvConverter csvConverter = new CsvConverter();
 
         csvConverter.ConvertStrategy(fullPath);
-
-        // Build the service provider
-        var serviceProvider = new ServiceCollection()
-            .AddDbContext<MyDbContext>(options =>
-                options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TradeApi;Integrated Security=True"))
-            .BuildServiceProvider();
-
-        // Use the database context
-        using (var context = serviceProvider.GetService<MyDbContext>())
-        {
-            // Perform database operations here
-        }
 
     }
 }
