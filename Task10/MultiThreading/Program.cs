1735022096349ad1bac7a9aaf020d325b5c685a9
StevenCK1 <stevencheung@hotmail.co.uk>
2023-06-27 23:31:25 +0100

    added lock to ensure thread safety

--- Task10/MultiThreading/Program.cs
+++ Task10/MultiThreading/Program.cs
@@ -1,4 +1,5 @@
-﻿using FuzzySharp;
+﻿using System.Diagnostics;
+using FuzzySharp;
 
 namespace MultiThreading
 {
@@ -7,12 +8,41 @@ namespace MultiThreading
         static void Main(string[] args)
         {
             VM _vm = new VM();
+            Stopwatch stopwatch = new Stopwatch();
 
 
             var results = new NamesByScore();
 
-            // Compare each name and calc score
-            for (var i = 0; i < _vm.Names.Count; i++)
+            //// Compare each name and calc score
+
+            //stopwatch.Start();
+
+            //for (var i = 0; i < _vm.Names.Count; i++)
+            //{
+            //    var resultToAdd = new NamesAndScore();
+            //    var name = _vm.Names[i].ToString();
+
+            //    for (var y = 0; y < _vm.Names.Count; y++)
+            //    {
+            //        var nameComparedAgainst = _vm.Names[y].ToString();
+            //        if (i != y)
+            //        {
+            //            var ratio = Fuzz.Ratio(_vm.Names[i].ToString(), _vm.Names[y].ToString());
+
+            //            var namesScore = new NamesAndScore() { name = name, nameCompared = nameComparedAgainst, score = ratio };
+            //            results.topScores.Add(namesScore);
+            //        }
+
+            //    }
+
+            //}
+
+            //stopwatch.Stop();
+
+            //stopwatch.Start();
+            object lockObject = new object(); // Object used as a lock
+
+            Parallel.For(0, _vm.Names.Count, (i) =>
             {
                 var resultToAdd = new NamesAndScore();
                 var name = _vm.Names[i].ToString();
@@ -25,14 +55,21 @@ namespace MultiThreading
                         var ratio = Fuzz.Ratio(_vm.Names[i].ToString(), _vm.Names[y].ToString());
 
                         var namesScore = new NamesAndScore() { name = name, nameCompared = nameComparedAgainst, score = ratio };
-                        results.topScores.Add(namesScore);
+
+                        lock (lockObject) // Lock the critical section
+                        {
+                            results.topScores.Add(namesScore);
+                        }
                     }
 
                 }
 
-            }
+            });
+            //stopwatch.Stop();
 
-           var sortedResults = results.topScores.OrderByDescending(name => name.score).ToList();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+            var sortedResults = results.topScores.OrderByDescending(name => name.score).ToList();
 
             foreach (var result in sortedResults)
             {
