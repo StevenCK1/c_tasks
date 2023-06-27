96055a0db8f2f101152a9065e4e0f96f98f226da
StevenCK1 <stevencheung@hotmail.co.uk>
2023-06-27 23:02:35 +0100

    Added functions to compare and sort

---
+++ Task10/MultiThreading/NamesByScore.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace MultiThreading
+{
+    public class NamesByScore
+    {
+        public List<NamesAndScore> topScores {get; set; } 
+
+        public NamesByScore()
+        {
+            topScores = new List<NamesAndScore>();
+        }
+    }
+
+    public class NamesAndScore
+    {
+        public string name { get; set; }
+        public string nameCompared { get; set; }
+        public int score { get; set; }
+    }
+}
--- Task10/MultiThreading/Program.cs
+++ Task10/MultiThreading/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace MultiThreading
+﻿using FuzzySharp;
+
+namespace MultiThreading
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            VM _vm = new VM();
+
+
+            var results = new NamesByScore();
+
+            // Compare each name and calc score
+            for (var i = 0; i < _vm.Names.Count; i++)
+            {
+                var resultToAdd = new NamesAndScore();
+                var name = _vm.Names[i].ToString();
+
+                for (var y = 0; y < _vm.Names.Count; y++)
+                {
+                    var nameComparedAgainst = _vm.Names[y].ToString();
+                    if (i != y)
+                    {
+                        var ratio = Fuzz.Ratio(_vm.Names[i].ToString(), _vm.Names[y].ToString());
+
+                        var namesScore = new NamesAndScore() { name = name, nameCompared = nameComparedAgainst, score = ratio };
+                        results.topScores.Add(namesScore);
+                    }
+
+                }
+
+            }
+
+           var sortedResults = results.topScores.OrderByDescending(name => name.score).ToList();
+
+            foreach (var result in sortedResults)
+            {
+                Console.WriteLine($"{result.score}: {result.name} -> {result.nameCompared}");
+            }
+
         }
     }
 }
