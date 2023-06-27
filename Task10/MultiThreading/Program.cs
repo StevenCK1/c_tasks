55e825667108e947622d38e7447336166b67a9b2
StevenCK1 <stevencheung@hotmail.co.uk>
2023-06-27 21:13:01 +0100

    created project for muti threading

---
+++ Task10/MultiThreading/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiThreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
