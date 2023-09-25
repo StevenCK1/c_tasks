22583652a1d49965dd0d0bd7cf41b11eef631682
StevenCK1 <stevencheung@hotmail.co.uk>
2023-09-25 18:47:39 +0100

    Added remove and update methods

--- Task15/Recursion/FormElementOperations.cs
+++ Task15/Recursion/FormElementOperations.cs
@@ -2,23 +2,68 @@
 {
     class FormElementOperations<T>
     {
-        public static void AddElement(FormElement<T> root, int parentId, T value)
+        public static bool AddElement(FormElement<T> root, int parentId, T value) // returns bool to indicate success or failure
         {
             if (root == null)
-                throw new ArgumentNullException(nameof(root), "Root element cannot be null.");
+                return false;
 
             if (root.Id == parentId)
             {
                 var newElement = new FormElement<T>(parentId, value);
                 root.Sections.Add(newElement);
+                return true;
             }
             else
             {
                 foreach (var section in root.Sections)
                 {
-                    AddElement(section, parentId, value);
+                    if (AddElement(section, parentId, value))
+                        return true;
                 }
             }
+
+            return false;
+        }
+
+        public static bool UpdateElement(FormElement<T> root, int id, T newValue)
+        {
+            if (root == null)
+                return false;
+
+            if (root.Id == id)
+            {
+                root.Value = newValue;
+                return true;
+            }
+
+            foreach (var section in root.Sections)
+            {
+                if (UpdateElement(section, id, newValue))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool RemoveElement(FormElement<T> root, int id)
+        {
+            if (root == null)
+                return false;
+
+            for (int i = 0; i < root.Sections.Count; i++)
+            {
+                var section = root.Sections[i];
+                if (section.Id == id)
+                {
+                    root.Sections.RemoveAt(i);
+                    return true;
+                }
+
+                if (RemoveElement(section, id))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
